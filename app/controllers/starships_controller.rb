adabaea1bc7e214358dc693da9dd48946ccf4f17
Vicente Castro <vocastro@uc.cl>
2019-03-21 21:29:17 -0300

    feat: starships show

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -1,6 +1,35 @@
 require 'rest-client'
 
 class StarshipsController < ApplicationController
+
   def show
+    id = params[:id]
+    response = RestClient.get "https://swapi.co/api/starships/#{id}"
+    json = JSON.parse response
+
+    @name = json['name']
+    @model = json['model']
+    @manufacturer = json['manufacturer']
+    @cost_in_credits = json['cost_in_credits']
+    @length = json['length']
+    @max_atmosphering_speed = json['max_atmosphering_speed']
+    @crew = json['crew']
+    @passengers = json['passengers']
+    @cargo_capacity = json['cargo_capacity']
+    @consumables = json['consumables']
+    @hyperdrive_rating = json['hyperdrive_rating']
+    @MGLT = json['MGLT']
+    @starship_class = json['starship_class']
+
+    @pilots = Array.new
+    json['pilots'].each do |url|
+        @pilots << (JSON.parse (RestClient.get url))
+    end
+
+    @films = Array.new
+    json['films'].each do |url|
+        @films << (JSON.parse (RestClient.get url))
+    end
   end
+
 end
