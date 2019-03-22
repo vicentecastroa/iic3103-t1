ae3c5a75559daf97abfbe7c5c41a8c9d2ce84f13
Vicente Castro <vocastro@uc.cl>
2019-03-21 21:03:43 -0300

    fix: arreglado id de personajes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'characters/:id', to: 'characters#show'
+
   root 'welcome#index'
+
   get '/films', to: 'films#show_all'
 
   get '/films/:id', to: 'films#show'
