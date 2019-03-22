9a569b5c562177a10fc20e8c22f2f6355d86cf0e
Vicente Castro <vocastro@uc.cl>
2019-03-21 21:40:28 -0300

    feat: planets init

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,4 @@
+class PlanetsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'planets/:id', to: 'planets#show'
+
   get 'starships/:id', to: 'starships#show'
 
   get 'characters/:id', to: 'characters#show'
